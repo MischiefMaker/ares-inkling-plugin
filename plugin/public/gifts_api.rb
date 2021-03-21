bf4e3044c5f75d1901f47a2becebaa1856577a78
MischiefMaker <59433210+MischiefMaker@users.noreply.github.com>
2021-03-21 03:23:17 -0400

    Add files via upload

--- plugin/public/gifts_api.rb
+++ plugin/public/gifts_api.rb
@@ -8,5 +8,25 @@ module AresMUSH
         }}
     end
 
+    def self.get_gifts_for_web_editing(char)
+        (char.gifts || {}).map { |name, desc| {
+        name: name,
+        desc: Website.format_input_for_html(desc)
+        }}
+    end
+
+    def self.save_gifts_from_chargen(char, chargen_data)
+      gifts = chargen_data[:custom] || {}
+      gifts.each do |name, desc|
+        gifts[name.titlecase] = Website.format_input_for_mush(desc)
+      end
+      char.update(gifts: gifts)
+      return []
+    end
+
+    def self.get_blurb_for_web()
+     blurb = Global.read_config('gifts')["gifts_blurb"]
+     Website.format_input_for_html(blurb)
+   end
   end
 end

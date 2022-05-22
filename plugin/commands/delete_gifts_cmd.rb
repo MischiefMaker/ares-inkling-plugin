ad07bcbf2e37dda6e2eb67468d8d0b15a5017d98
MischiefMaker <59433210+MischiefMaker@users.noreply.github.com>
2022-05-21 22:09:30 -0400

    Update delete_gifts_cmd.rb

--- plugin/commands/delete_gifts_cmd.rb
+++ plugin/commands/delete_gifts_cmd.rb
@@ -1,40 +1,40 @@
-module AresMUSH
-  module Gifts
-    class DeleteGiftsCmd
+inklingmodule AresMUSH
+  module inklings
+    class DeleteInklingsCmd
     include CommandHandler
 
-     attr_accessor :name, :gift_name, :gift_description
+     attr_accessor :name, :inkling_name, :inkling_description
 
      def parse_args
        args = cmd.parse_args(ArgParser.arg1_equals_optional_arg2)
         if (args.arg2 == nil)
           self.name = enactor_name
-          self.gift_name = titlecase_arg(args.arg1)
+          self.inkling_name = titlecase_arg(args.arg1)
         else
           self.name = titlecase_arg(args.arg1)
-          self.gift_name = titlecase_arg(args.arg2)
+          self.inkling_name = titlecase_arg(args.arg2)
         end
     end
 
     def required_args
-      [ self.name, self.gift_name ]
+      [ self.name, self.inkling_name ]
     end
 
     def check_can_view
        return nil if self.name == enactor_name
        return nil if enactor.has_permission?("view_bgs")
-       return "You're not allowed to edit other people's gifts."
+       return "You're not allowed to edit other people's inklings."
     end
 
     def handle
       char = Character.find_one_by_name(self.name)
-      gifts = Hash[char.gifts]
-      if ( gifts.has_key?(self.gift_name) )
-        gifts.delete self.gift_name
-        char.update(gifts: gifts)
-        client.emit_success "Gift #{self.gift_name} deleted from #{self.name}"
+      inklings = Hash[char.inklings]
+      if ( inklings.has_key?(self.inkling_name) )
+        inklings.delete self.inkling_name
+        char.update(inklings: inklings)
+        client.emit_success "Inkling #{self.inkling_name} deleted from #{self.name}"
       else
-        client.emit_failure "#{self.name} does not have a Gift named #{self.gift_name}"
+        client.emit_failure "#{self.name} does not have an Inkling named #{self.inkling_name}"
       end
     end
 

a690e2442b9f2dbef316145a2771e505a1476b18
MischiefMaker <59433210+MischiefMaker@users.noreply.github.com>
2022-05-21 22:10:36 -0400

    Update edit_gifts_cmd.rb

--- plugin/commands/edit_gifts_cmd.rb
+++ plugin/commands/edit_gifts_cmd.rb
@@ -1,42 +1,42 @@
 module AresMUSH
-  module Gifts
-    class EditGiftsCmd
+  module Inklings
+    class EditInklingsCmd
     include CommandHandler
 
-     attr_accessor :name, :gift_name, :gift_description
+     attr_accessor :name, :inkling_name, :inkling_description
 
      def parse_args
        args = cmd.parse_args(ArgParser.arg1_equals_optional_arg2)
         if (args.arg2 == nil)
           self.name = enactor_name
-          self.gift_name = titlecase_arg(args.arg1)
+          self.inkling_name = titlecase_arg(args.arg1)
         else
           self.name = titlecase_arg(args.arg1)
-          self.gift_name = titlecase_arg(args.arg2)
+          self.inkling_name = titlecase_arg(args.arg2)
         end
     end
 
     def required_args
-      [ self.name, self.gift_name ]
+      [ self.name, self.inkling_name ]
     end
 
     def check_can_view
        return nil if self.name == enactor_name
        return nil if enactor.has_permission?("view_bgs")
-       return "You're not allowed to edit other people's gifts."
+       return "You're not allowed to edit other people's Inklings."
     end
 
     def handle
       char = Character.find_one_by_name(self.name)
-      if (char.gifts == nil)
-        client.emit_failure "#{self.name} does not have any gifts. Use gifts/set to create some first!"
+      if (char.inklings == nil)
+        client.emit_failure "#{self.name} does not have any Inklings. Use inklings/set to create some first!"
       else
-        gifts = Hash[char.gifts]
-        self.gift_description = gifts[self.gift_name]
+        inklings = Hash[char.inklings]
+        self.inkling_description = inklings[self.inkling_name]
           if (enactor.name == self.name)
-            Utils.grab client, enactor, "gifts/set #{self.gift_name}=#{self.gift_description}"
+            Utils.grab client, enactor, "inklings/set #{self.inkling_name}=#{self.inkling_description}"
           else
-            Utils.grab client, enactor, "gifts/set #{self.name}=#{self.gift_name}/#{self.gift_description}"
+            Utils.grab client, enactor, "inklings/set #{self.name}=#{self.inkling_name}/#{self.inkling_description}"
           end
       end
     end
